8893a45274363a47f79e5a28d8b84b945f4465a6
javier1991vargas <104287410+javier1991vargas@users.noreply.github.com>
2022-04-24 13:51:08 -0500

    Add files via upload

---
+++ Utilidades/AutoMaperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WebApAutores.DTOs;
+using WebApAutores.Entidades;
+
+namespace WebApAutores.Utilidades
+{
+    public class AutoMaperProfiles :Profile
+    {
+        public AutoMaperProfiles()
+        {
+            //desde donde queremos mapear-->(AutorCreacionDTOs) y el destino-->(Autor)
+            CreateMap<AutorCreacionDTOs, Autor>();
+            CreateMap<Autor, AutorDTO>();
+            CreateMap<LibroCreacionDTO, Libro>();
+            CreateMap<Libro, LibroDTO>();
+            CreateMap<ComentariosCreacionDTO,Comentarios>();
+            CreateMap<Comentarios, ComentarioDTO>();
+         }
+    }
+}
